77397ee3f233a41ced4e8eabb65fa50a67bcec89
SpawnCampGames <60554748+SpawnCampGames@users.noreply.github.com>
2020-09-04 18:16:19 -0500

    Add files via upload

---
+++ SimpleFPS.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFPS : MonoBehaviour
+{
+    public int avgFrameRate;
+
+    public void Update()
+    {
+        int current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = current;
+
+        Debug.Log(avgFrameRate);
+    }
+}
