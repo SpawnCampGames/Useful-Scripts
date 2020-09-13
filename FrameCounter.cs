085c573a9326747690ef2f2bc412fb3d8a0fa3e0
SpawnCampGames <60554748+SpawnCampGames@users.noreply.github.com>
2020-09-12 21:18:04 -0500

    Add files via upload

---
+++ FrameCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using TMPro;
+
+public class FrameCounter : MonoBehaviour
+{
+    TextMeshProUGUI frameRateText;
+    private int frameRateInteger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frameRateText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameRateInteger = Convert.ToInt32(1.0f / Time.deltaTime);
+        frameRateText.text = frameRateInteger.ToString();
+    }
+}
